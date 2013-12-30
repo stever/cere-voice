52baff65156ca882e03708515efe25243a9680e9
Steven Robertson <steverobertson@gmail.com>
2013-12-30 17:16:43 +0000

    Adding a few JavaScript libs

---
+++ scripts/bootstrap.ts
@@ -0,0 +1,19 @@
+/// <reference path="../typedefs/require.d.ts" />
+
+require.config({
+    baseUrl: '/scripts',
+    paths: {
+        templates: '../templates',
+        text: '../lib/requirejs-text-2.0.10',
+        jquery: '../lib/jquery-1.10.2',
+        handlebars: '../lib/handlebars-1.2.0',
+        main: '../scripts/main',
+    },
+    shim: {
+        jquery: {
+            exports: '$',
+        },
+    },
+});
+
+require(['main']);
